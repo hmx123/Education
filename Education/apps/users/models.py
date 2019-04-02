141e1aa8c8b337538d6c9a931d248fbf8c29ae6f
hmx123 <17839192461@163.com>
2019-04-02 15:34:20 +0800

    Revert "后台功能实现"
    This reverts commit a852308

--- Education/apps/users/models.py
+++ Education/apps/users/models.py
@@ -1,21 +1,23 @@
-from datetime import datetime
-
+from _datetime import datetime
 from django.db import models
+
+# Create your models here.
 from django.contrib.auth.models import AbstractUser
+
 
 class UserProfile(AbstractUser):
 
     gender_choices = (
-        ('male','男'),
-        ('female','女')
+        ('male', '男'),
+        ('female', '女')
     )
 
-    nick_name = models.CharField('昵称',max_length=50,default='')
-    birthday = models.DateField('生日',null=True,blank=True)
-    gender = models.CharField('性别',max_length=10,choices=gender_choices,default='female')
-    adress = models.CharField('地址',max_length=100,default='')
-    mobile = models.CharField('手机号',max_length=11,null=True,blank=True)
-    image = models.ImageField(upload_to='image/%Y%m',default='image/default.png',max_length=100)
+    nick_names = models.CharField('昵称', max_length=50, default='')
+    birthday = models.DateField('生日', null=True, blank=True)
+    adress = models.CharField('地址', max_length=100, default='')
+    mobile = models.CharField('手机号', max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to='image/%Y%m', default='image/default.png', max_length=100)
+
 
     class Meta:
         verbose_name = '用户信息'
@@ -25,16 +27,17 @@ class UserProfile(AbstractUser):
         return self.username
 
 
-class EmailVerifyRecord(models.Model):
+class EmaiVerifyRecord(models.Model):
     send_choices = (
-        ('register','注册'),
-        ('forget','找回密码')
+        ('register', '注册'),
+        ('forget', '找回密码')
     )
 
-    code = models.CharField('验证码',max_length=20)
-    email = models.EmailField('邮箱',max_length=50)
-    send_type = models.CharField(choices=send_choices,max_length=10)
-    send_time = models.DateTimeField(default=datetime.now)
+    code = models.CharField('验证码', max_length=20)
+    email = models.CharField('邮箱', max_length=50)
+    send_type = models.CharField(choices=send_choices, max_length=10)
+    send_time = models.DateTimeField(default=datetime.now())
+
 
     class Meta:
         verbose_name = '邮箱验证码'
@@ -51,3 +54,5 @@ class Banner(models.Model):
     class Meta:
         verbose_name = '轮播图'
         verbose_name_plural = verbose_name
+
+
