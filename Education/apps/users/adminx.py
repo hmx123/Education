7152f85e4e96dddfb7b4b3e99208c49976089b77
hmx123 <17839192461@163.com>
2019-04-03 10:29:10 +0800

    后台功能增加

---
+++ Education/apps/users/adminx.py
@@ -0,0 +1,22 @@
+import xadmin
+
+from .models import EmaiVerifyRecord,Banner
+
+#xadmin中这里是继承object，不再是继承admin
+class EmailVerifyRecordAdmin(object):
+    # 显示的列
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 搜索的字段
+    search_fields = ['code', 'email', 'send_type']
+    # 过滤
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url','index', 'add_time']
+    search_fields = ['title', 'image', 'url','index']
+    list_filter = ['title', 'image', 'url','index', 'add_time']
+
+
+xadmin.site.register(EmaiVerifyRecord,EmailVerifyRecordAdmin)
+xadmin.site.register(Banner,BannerAdmin)
